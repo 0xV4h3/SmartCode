b750974ecafbad0eaa7c227f3028a9bb54c89b4f
Վահե Բաբայան <vahebabayan.tt219-2@polytechnic.am>
2025-08-23 16:35:04 +0400

    changes

--- Python/Lessons/lesson22/homework/leetcode/leetcode3197.py
+++ Python/Lessons/lesson22/homework/leetcode/leetcode3197.py
@@ -71,5 +71,3 @@ if __name__ == "__main__":
     sol = Solution()
     print(sol.minimumSum(grid = [[1,0,1],[1,1,1]])) # 5
     print(sol.minimumSum(grid = [[1,0,1,0],[0,1,0,1]]))  # 5
-
-    TOKEN = "8267938861:AAELlm-fJiErDjQn-NdrcppZ6M4asCuy2ys"
